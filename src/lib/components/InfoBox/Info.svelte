7fada9c45380388356eddb2cf7a207d550f8cc93
Mehdi ELOUALY <92476321+meel-hd@users.noreply.github.com>
2023-05-05 23:14:47 +0100

    Update version from 0.0.1 to 0.0.2

--- src/lib/components/InfoBox/Info.svelte
+++ src/lib/components/InfoBox/Info.svelte
@@ -48,7 +48,7 @@
           <img id="app-logo" src="/app-icon.png" alt="" />
           <div>
             <h1>Lofi Engine</h1>
-            <p id="version">Version 0.0.1</p>
+            <p id="version">Version 0.0.2</p>
             <p id="tagline">
               Create your own atmosphere, Lofi Engine sets the mood.
             </p>
