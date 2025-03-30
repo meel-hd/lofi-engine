0782698dc960667b49b017e976b9b7253b03a4be
Mehdi Eloualy <92476321+meel-hd@users.noreply.github.com>
2025-03-30 17:26:56 +0000

    Hide social links

--- src/lib/components/InfoBox/SocialLinks.svelte
+++ src/lib/components/InfoBox/SocialLinks.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import {
-    IconBrandInstagram,
-    IconBrandPatreon,
-    IconBrandTwitter,
-    IconLink,
+    IconBrandGithub,
+    // IconBrandInstagram,
+    // IconBrandPatreon,
+    // IconBrandTwitter,
+    // IconLink,
   } from "@tabler/icons-svelte";
 </script>
 
 <div class="social-links">
-  <a href="https://twitter.com" target="_blank">
+  <!-- <a href="https://twitter.com" target="_blank">
     <IconBrandTwitter size={25} />
   </a>
   <a href="https://patreon.com" target="_blank">
@@ -16,6 +17,9 @@
   </a>
   <a href="https://LofiEngine.com" target="_blank">
     <IconLink size={25} />
+  </a> -->
+  <a href="https://github.com/meel-hd/lofi-engine" target="_blank">
+    <IconBrandGithub size={25} />
   </a>
 </div>
 
