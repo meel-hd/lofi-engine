0590bfbe6d1b6cfe0b6bb668fa770e2ddd1cfd0c
werserk <werserk@inbox.ru>
2025-06-07 11:56:46 +0300

    fix silent sound

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import sveltePreprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  base: "./",
   plugins: [
     svelte({
       preprocess: [
